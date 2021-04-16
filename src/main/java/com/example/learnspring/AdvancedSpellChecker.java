6faa033df619a8703fbf16601b94418665546e7b
user <user@WINDOWS-VK4C0HF>
2021-04-16 18:13:54 +0200

    Read External Properties.

--- src/main/java/com/example/learnspring/AdvancedSpellChecker.java
+++ src/main/java/com/example/learnspring/AdvancedSpellChecker.java
@@ -2,11 +2,18 @@ package com.example.learnspring;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource(value = "classpath:/application.properties")
 public class AdvancedSpellChecker implements SpellChecker, InitializingBean, DisposableBean {
+
+    @Value("${app.database.uri}")
+    private String databaseUri;
+
     @Override
     public void checkSpelling(String emailMessage) {
         if (emailMessage != null) {
@@ -14,6 +21,8 @@ public class AdvancedSpellChecker implements SpellChecker, InitializingBean, Dis
             // Check grammatical errors in the email message
             // Check spellings in the email message
             System.out.println("Spell Checking Completed!!!");
+
+            System.out.println("DB URI: " + databaseUri);
         } else {
             throw new RuntimeException("An exception occurred while checking spelling!");
         }
