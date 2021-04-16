e111ddb8dbd01efd7b253dffe069da9d9fc637ed
user <user@WINDOWS-VK4C0HF>
2021-04-16 17:28:40 +0200

    Implement Dependency Injection using Java Configuration.

---
+++ src/main/java/com/example/learnspring/AppConfig.java
@@ -0,0 +1,23 @@
+package com.example.learnspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "basicSpellChecker")
+    public BasicSpellChecker createBasicSpellChecker() {
+        return new BasicSpellChecker();
+    }
+
+    @Bean(name = "advancedSpellChecker")
+    public AdvancedSpellChecker createAdvancedSpellChecker() {
+        return new AdvancedSpellChecker();
+    }
+
+    @Bean(name = "emailClient")
+    public EmailClient createEmailClient() {
+        return new EmailClient(createBasicSpellChecker());
+    }
+}
--- src/main/java/com/example/learnspring/LearnSpringApplication.java
+++ src/main/java/com/example/learnspring/LearnSpringApplication.java
@@ -1,12 +1,12 @@
 package com.example.learnspring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class LearnSpringApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
         emailClient.sendEmail("Hey! This is my first message.");
         emailClient.sendEmail("Hey! This is my second message.");
