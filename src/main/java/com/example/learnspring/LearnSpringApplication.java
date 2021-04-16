4b308d4c811cfd83585095fae141c4cbca1bc566
user <user@WINDOWS-VK4C0HF>
2021-04-16 15:51:08 +0200

    Implement Dependency Injection using ApplicationContext and XML beans configuration.

--- src/main/java/com/example/learnspring/LearnSpringApplication.java
+++ src/main/java/com/example/learnspring/LearnSpringApplication.java
@@ -1,9 +1,13 @@
 package com.example.learnspring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LearnSpringApplication {
 
     public static void main(String[] args) {
-        EmailClient emailClient = new EmailClient(new BasicSpellChecker());
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
         emailClient.sendEmail("Hey! This is my first message.");
         emailClient.sendEmail("Hey! This is my second message.");
     }
